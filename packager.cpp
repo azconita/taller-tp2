3eded866084e5d4478b58855c602edb8f72b194d
azconita <gabriela.azcona@despegar.com>
2018-04-16 04:52:09 -0300

    working

--- packager.cpp
+++ packager.cpp
@@ -2,8 +2,8 @@
 
 
 ScrewClassifier::ScrewClassifier(const char* filename,
-                  ScrewsQueue *classified, ErrPrinter *pe, OutPrinter *po) :
-                  perr(pe), pout(po),
+                  ScrewsQueue *classified) :
+
                   classified(classified){
   this->infile = std::ifstream(filename, std::ios::in | std::ios::binary);
   if (this->infile.fail()) {
@@ -43,17 +43,13 @@ ScrewClassifier::~ScrewClassifier() {
   this->infile.close();
 }
 
-ScrewPackager::ScrewPackager (const char * cfg_file) : config(cfg_file) {
-  this->perr = new ErrPrinter();
-  this->pout = new OutPrinter();
-}
 
 
 void ScrewPackager::connect_to_classificators(const int total,
                                               const char *file_classif[]) {
   this->total = total;
   for (size_t i = 0; i < total; i++) {
-    this->classificators.push_back(ScrewClassifier(file_classif[i], &(this->classified), &(this->perr), &(this->pout)));
+    this->classificators.push_back(ScrewClassifier(file_classif[i], &(this->classified)));
   }
 }
 
@@ -69,32 +65,31 @@ void ScrewPackager::start() {
 void ScrewPackager::make_packages() {
   std::map<int, ScrewsUnpackaged>::iterator it;
   bool finish = false;
-  while (finish == false) {
+  while (finish == false || !this->classified.empty()) {
     ScrewBlock block = this->classified.pop();
-    this->classified.push(block);
     it = this->screws_unpackaged.find(block.type);
-    if (it != this->screws_unpackaged.end()) {
+    if (it == this->screws_unpackaged.end()) {
+      this->screws_unpackaged.emplace(block.type,ScrewsUnpackaged(block.type, this->config.get_limit(block.type)));
+      it = this->screws_unpackaged.find(block.type);
+    }
       if (this->config.has_type(block.type)) {
-        int p = it->second.make_package(block.quantity, block.width, this->config.get_limit(block.type), this->config.get_type(block.type));
-        for (size_t j = 0; j < p; j++) {
-          std::stringstream s;
-          s << "Paquete listo: " << this->config.get_limit(block.type) << " tornillos de tipo \n";
-                  //<< btype << " (mediana: " << m << ")\n";
-          std::cout << s.str();
+        for (size_t j = 0; j < block.quantity; j++) {
+          int m = it->second.make_package(block.quantity, block.width, this->config.get_limit(block.type), this->config.get_type(block.type));
+          if (m > 0)
+            std::cout << "Paquete listo: " << this->config.get_limit(block.type) << " tornillos de tipo "
+                  << this->config.get_type(block.type) << " (mediana: " << m << ")\n";
         }
       } else {
         std::cerr << "Tipo de tornillo invalido: " << block.type << '\n';
       }
-      this->classified.pop();
-    } else {
-      this->screws_unpackaged.emplace(block.type,ScrewsUnpackaged(block.type, this->config.get_limit(block.type)), &(this->perr), &(this->pout));
+    if (this->classified.empty()) {
+      bool checkthreads = true;
+      for (size_t i = 0; i < this->classificators.size(); i++) {
+        if (this->classificators[i].is_finished() == false)
+          checkthreads = false;
+      }
+      finish = checkthreads;
     }
-    bool checkthreads = true;
-    for (size_t i = 0; i < this->classificators.size(); i++) {
-      if (this->classificators[i].is_finished() == false)
-        checkthreads = false;
-    }
-    finish = checkthreads;
   }
 }
 
@@ -122,6 +117,4 @@ ScrewPackager::~ScrewPackager() {
   for (size_t i = 0; i < this->classificators.size(); i++) {
     this->classificators[i].join();
   }
-  delete this->perr;
-  delete this->pout;
 }
